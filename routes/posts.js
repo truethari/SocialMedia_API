a7ef86f7ee2e6113c4c7706249a9f76e108fe3ff
Tharindu N <tharindu.nm@yahoo.com>
2023-03-11 13:42:56 +0530

    checkPostExists, checkCommentExists

--- routes/posts.js
+++ routes/posts.js
@@ -11,16 +11,15 @@ router.get("/", (req, res) => {
     res.json(posts);
 });
 
-router.get("/:id", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
+const middleware = {
+    checkPostExists,
+    checkCommentExists,
+};
 
-    if (found) {
-        res.json(posts.filter((post) => post.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
+router.get("/:id", checkPostExists, (req, res) => {
+    checkPostExists(req, res);
+
+    res.json(posts.filter((post) => post.id === parseInt(req.params.id)));
 });
 
 router.post("/", (req, res) => {
@@ -42,182 +41,106 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
+    const { error } = validatePost(req.body);
 
-    if (found) {
-        const { error } = validatePost(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
-        if (error) {
-            return res.status(400).send(error.details[0].message);
+    const updatedPost = req.body;
+    posts.forEach((post) => {
+        if (post.id === parseInt(req.params.id)) {
+            post.user = updatedPost.user ? updatedPost.user : post.user;
+            post.body = updatedPost.body ? updatedPost.body : post.body;
+            post.tags = updatedPost.tags ? updatedPost.tags : post.tags;
+
+            res.json({
+                msg: "Post updated",
+                post,
+            });
         }
+    });
+});
 
-        const updatedPost = req.body;
-        posts.forEach((post) => {
-            if (post.id === parseInt(req.params.id)) {
-                post.user = updatedPost.user ? updatedPost.user : post.user;
-                post.body = updatedPost.body ? updatedPost.body : post.body;
-                post.tags = updatedPost.tags ? updatedPost.tags : post.tags;
+router.delete("/:id", checkPostExists, (req, res) => {
+    res.json({
+        msg: "Post deleted",
+        posts: posts.filter((post) => post.id !== parseInt(req.params.id)),
+    });
+});
+
+router.get("/:id/comments", checkPostExists, (req, res) => {
+    res.json(
+        comments.filter((comment) => comment.post === parseInt(req.params.id))
+    );
+});
+
+router.get(
+    "/:id/comments/:commentId",
+    [middleware.checkPostExists, middleware.checkCommentExists],
+    (req, res) => {
+        const comment = comments.filter(
+            (comment) =>
+                comment.post === parseInt(req.params.id) &&
+                comment.id === parseInt(req.params.commentId)
+        );
+
+        res.json(comment);
+    }
+);
+
+router.post("/:id/comments", checkPostExists, (req, res) => {
+    const newComment = {
+        id: comments.length + 1,
+        post: parseInt(req.params.id),
+        user: req.body.user,
+        body: req.body.body,
+    };
+
+    comments.push(newComment);
+    res.json(comments);
+});
+
+router.put(
+    "/:id/comments/:commentId",
+    [middleware.checkPostExists, middleware.checkCommentExists],
+    (req, res) => {
+        const updatedComment = req.body;
+        comments.forEach((comment) => {
+            if (
+                comment.post === parseInt(req.params.id) &&
+                comment.id === parseInt(req.params.commentId)
+            ) {
+                comment.user = updatedComment.user
+                    ? updatedComment.user
+                    : comment.user;
+                comment.body = updatedComment.body
+                    ? updatedComment.body
+                    : comment.body;
 
                 res.json({
-                    msg: "Post updated",
-                    post,
+                    msg: "Comment updated",
+                    comment,
                 });
             }
         });
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
     }
-});
+);
 
-router.delete("/:id", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
+router.delete(
+    "/:id/comments/:commentId",
+    [middleware.checkPostExists, middleware.checkCommentExists],
+    (req, res) => {
         res.json({
-            msg: "Post deleted",
-            posts: posts.filter((post) => post.id !== parseInt(req.params.id)),
-        });
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
-});
-
-router.get("/:id/comments", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
-        res.json(
-            comments.filter(
-                (comment) => comment.post === parseInt(req.params.id)
-            )
-        );
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
+            msg: "Comment deleted",
+            comments: comments.filter(
+                (comment) =>
+                    comment.post !== parseInt(req.params.id) &&
+                    comment.id !== parseInt(req.params.commentId)
+            ),
         });
     }
-});
-
-router.get("/:id/comments/:commentId", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
-        const comment = comments.filter(
-            (comment) =>
-                comment.post === parseInt(req.params.id) &&
-                comment.id === parseInt(req.params.commentId)
-        );
-
-        if (comment.length > 0) {
-            res.json(comment);
-        } else {
-            res.status(400).json({
-                msg: `No comment with the id of ${req.params.commentId}`,
-            });
-        }
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
-});
-
-router.post("/:id/comments", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
-        const newComment = {
-            id: comments.length + 1,
-            post: parseInt(req.params.id),
-            user: req.body.user,
-            body: req.body.body,
-        };
-
-        comments.push(newComment);
-        res.json(comments);
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
-});
-
-router.put("/:id/comments/:commentId", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
-        const comment = comments.filter(
-            (comment) =>
-                comment.post === parseInt(req.params.id) &&
-                comment.id === parseInt(req.params.commentId)
-        );
-
-        if (comment.length > 0) {
-            const updatedComment = req.body;
-            comments.forEach((comment) => {
-                if (
-                    comment.post === parseInt(req.params.id) &&
-                    comment.id === parseInt(req.params.commentId)
-                ) {
-                    comment.user = updatedComment.user
-                        ? updatedComment.user
-                        : comment.user;
-                    comment.body = updatedComment.body
-                        ? updatedComment.body
-                        : comment.body;
-
-                    res.json({
-                        msg: "Comment updated",
-                        comment,
-                    });
-                }
-            });
-        } else {
-            res.status(400).json({
-                msg: `No comment with the id of ${req.params.commentId}`,
-            });
-        }
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
-});
-
-router.delete("/:id/comments/:commentId", (req, res) => {
-    const found = posts.some((post) => post.id === parseInt(req.params.id));
-
-    if (found) {
-        const comment = comments.filter(
-            (comment) =>
-                comment.post === parseInt(req.params.id) &&
-                comment.id === parseInt(req.params.commentId)
-        );
-
-        if (comment.length > 0) {
-            res.json({
-                msg: "Comment deleted",
-                comments: comments.filter(
-                    (comment) =>
-                        comment.post !== parseInt(req.params.id) &&
-                        comment.id !== parseInt(req.params.commentId)
-                ),
-            });
-        } else {
-            res.status(400).json({
-                msg: `No comment with the id of ${req.params.commentId}`,
-            });
-        }
-    } else {
-        res.status(400).json({
-            msg: `No post with the id of ${req.params.id}`,
-        });
-    }
-});
+);
 
 function validatePost(post) {
     const schema = Joi.object({
@@ -228,6 +151,34 @@ function validatePost(post) {
     });
 
     return schema.validate(post);
+}
+
+function checkPostExists(req, res, next) {
+    const post = posts.find((post) => post.id === parseInt(req.params.id));
+
+    if (!post) {
+        return res.status(404).json({
+            msg: `No post with the id of ${req.params.id}`,
+        });
+    } else {
+        next();
+    }
+}
+
+function checkCommentExists(req, res, next) {
+    const comment = comments.find(
+        (comment) =>
+            comment.post === parseInt(req.params.id) &&
+            comment.id === parseInt(req.params.commentId)
+    );
+
+    if (!comment) {
+        return res.status(404).json({
+            msg: `No comment with the id of ${req.params.commentId}`,
+        });
+    } else {
+        next();
+    }
 }
 
 module.exports = router;
