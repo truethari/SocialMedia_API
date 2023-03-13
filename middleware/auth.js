28b95aea63a19f892c129beb588851acbec9a0ba
Tharindu N <tharindu.nm@yahoo.com>
2023-03-13 08:24:34 +0530

    Remove auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,22 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-function auth(req, res, next) {
-    const token = req.header("x-auth-token");
-    if (!token)
-        return res.status(401).send("Access denied. No token provided.");
+// function auth(req, res, next) {
+//     const token = req.header("x-auth-token");
+//     if (!token)
+//         return res.status(401).send("Access denied. No token provided.");
 
-    try {
-        const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
-        req.user = decoded;
-        next();
-    } catch (ex) {
-        res.status(400).send("Invalid token.");
-    }
+//     try {
+//         const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
+//         req.user = decoded;
+//         next();
+//     } catch (ex) {
+//         res.status(400).send("Invalid token.");
+//     }
+// }
+
+function auth(req, res, next) {
+    next();
 }
 
 module.exports = auth;
