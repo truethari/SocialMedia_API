fc4f1d7a34c60dface3408e8c395ad8e4740e06c
Tharindu N <tharindu.nm@yahoo.com>
2023-03-20 23:16:07 +0530

    Check if the user exists when auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-function auth(req, res, next) {
+const { User } = require("../models/user");
+
+async function auth(req, res, next) {
     const token = req.header("x-auth-token");
 
     if (!token)
@@ -12,6 +14,15 @@ function auth(req, res, next) {
     try {
         const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
         req.user = decoded;
+
+        const user = await User.findOne({ _id: req.user._id });
+
+        if (!user) {
+            return res.status(401).json({
+                msg: `Invalid token`,
+            });
+        }
+
         next();
     } catch (ex) {
         res.status(400).json({ msg: "Invalid token." });
