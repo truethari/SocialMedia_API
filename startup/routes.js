2e7632229ab4d8790007a9b2220b45a3746ab823
Tharindu N <tharindu.nm@yahoo.com>
2023-03-26 22:37:51 +0530

    helmet

--- startup/routes.js
+++ startup/routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 
 const error = require("../middleware/error");
 const login = require("../routes/login");
@@ -8,6 +9,7 @@ const posts = require("../routes/posts");
 module.exports = function (app) {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+    app.use(helmet());
     app.use("/api/users", users);
     app.use("/api/posts", posts);
     app.use("/api/login", login);
